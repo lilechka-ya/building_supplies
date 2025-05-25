d15cd5601fcb666dfb272f7a9dbf49c916b81051
Lilechka <liliana.soyenkova@mail.ru>
2025-05-25 20:32:12 +0300

    Добавлены изменения в building_supplies_group.py

--- building_supplies_group.py
+++ building_supplies_group.py
@@ -14,13 +14,32 @@ class OrderManager:
     def read_from_file(self, filename):
         self.orders = {}
         self.count = 0
+        orders_list = []
+        
         with open(filename, 'r', encoding='utf8') as file:
             for line in file:
                 if line.strip():
                     parts = line.strip().split(', ')
                     if len(parts) == 5:
-                        self.orders[self.count] = Order(*parts)
-                        self.count += 1
+                        orders_list.append(Order(*parts))
+        
+        # Сортировка пузырьком по фамилии
+        self.bubble_sort_by_surname(orders_list)
+        
+        for idx, order in enumerate(orders_list):
+            self.orders[idx] = order
+            self.count += 1
+    
+    def bubble_sort_by_surname(self, orders_list):
+        n = len(orders_list)
+        for i in range(n - 1):
+            for j in range(0, n - i - 1):
+                # Сравниваем фамилии (первое слово в fio)
+                surname1 = orders_list[j].fio.split()[0].lower()
+                surname2 = orders_list[j + 1].fio.split()[0].lower()
+                if surname1 > surname2:
+                    # Меняем элементы местами
+                    orders_list[j], orders_list[j + 1] = orders_list[j + 1], orders_list[j]
     
     def update_file(self, filename, new_data):
         with open(filename, 'w', encoding='utf8') as file:
